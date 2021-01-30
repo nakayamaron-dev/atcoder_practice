1008b4777e9c1396e28ae1066df5d984419afb8a
Ryo Nakayama <nakayamadevelop@gmail.com>
2021-01-30 16:15:58 +0900

    保存漏れ

--- utility.py
+++ utility.py
@@ -1,5 +1,6 @@
 import math
 import itertools
+from functools import reduce
 
 # 階乗計算
 def calc_factorial(n):
@@ -40,3 +41,30 @@ def splitdigit(num_str):
 # mod計算
 def calc_mod(n, mod):
     return n % mod
+
+# 最大公約数
+def gcd_list(num_list: list) -> int:
+    return reduce(gcd, num_list)
+
+# 最小公倍数
+def lcm_base(x: int, y: int) -> int:
+    return (x * y) // gcd(x, y)
+def lcm_list(num_list: list):
+    return reduce(lcm_base, num_list, 1)
+
+# 素因数分解
+def prime_factorize(n: int) -> list:
+    return_list = []
+    while n % 2 == 0:
+        return_list.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            return_list.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        return_list.append(n)
+    return return_list
