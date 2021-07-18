c9dc84335842b95aa75659954d9406626d6f293a
Ryo Nakayama <nakayamadevelop@gmail.com>
2021-07-19 05:21:14 +0900

    typical-3

---
+++ typical90/003/3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from collections import deque
+def main():
+    N = int(input())
+    g = [[] for _ in range(N)]
+
+    for _ in range(N-1):
+        A, B = map(int,input().split())
+        A, B = A-1, B-1
+        g[A].append(B)
+        g[B].append(A)
+
+    def bfs(x):
+        q = deque()
+        q.append(x)
+        dist = [-1] * N
+        dist[x] = 0
+
+        while q:
+            v = q.popleft()
+
+            for i in g[v]:
+                if dist[i] != -1:
+                    continue
+
+                dist[i] = dist[v] + 1
+                q.append(i)
+        
+        return dist
+    
+    # start地点を決めるためのbfs
+    d = bfs(0)
+    start = d.index(max(d))
+
+    # 決めたstartから始めた場合の最大距離を求める。
+    d = bfs(start)
+
+    return max(d) + 1
+
+print(main())
+
