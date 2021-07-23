99737777a57f736f8186d3c39fe26b5b2e8a04b6
Ryo Nakayama <nakayamadevelop@gmail.com>
2021-07-23 14:37:08 +0900

    typical-55

---
+++ typical90/055/55.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def main():
+    N, P, Q = map(int,input().split())
+    A = list(map(int, input().split()))
+    ans = 0
+
+    for i in range(N):
+        for j in range(i+1, N):
+            for k in range(j+1, N):
+                for l in range(k+1, N):
+                    for m in range(l+1, N):
+                        if A[i] * A[j] % P * A[k] % P * A[l] % P * A[m] % P == Q:
+                            ans += 1
+    
+    return ans
+
+print(main())
