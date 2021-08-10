5a9f689f461e8458d1c59cfa15c14b1bed422d21
Ryo Nakayama <nakayamadevelop@gmail.com>
2021-08-11 07:06:26 +0900

    復習 abc125_c

--- practice/abc125/c/c3.py
+++ practice/abc125/c/c3.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
+def get_gcd(*numbers):
+    import math
+    from functools import reduce
+    return reduce(math.gcd, numbers)
+
 def main():
     N = int(input())
     A = list(map(int, input().split()))
+    ans = []
+    l, r = [0], [0]
+
+    for i in range(N):
+
+        # l[i]: A1 ~ Ai-1の最大公約数
+        l.append(get_gcd(l[i], A[i]))
+
+        # r[i]: Ai ~ Anの最大公約数
+        r.append(get_gcd(r[i], A[N-1-i]))
+    
+    r = r[::-1]
+    for i in range(N):
+        ans.append(get_gcd(l[i], r[i+1]))
+    
+    return max(ans)
+
+print(main())
+
+# 最大公約数の計算は順不同
+# 最大公約数計算＋累積の考え方で計算量削減
